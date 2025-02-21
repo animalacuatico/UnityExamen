3ec048ab457c2de6679adaac9868b786739f9006
aquatic_animal <angelocaceda333@gmail.com>
2025-02-21 15:56:16 +0100

    APARTADO 3
    Realizado el witchTime

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WitchTime : MonoBehaviour
 {
-    // Update is called once per frame
+    public float slowmoTime = 1.0f, slowedTime = 0.25f;
+    public AudioClip witchTime;
+
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(SlowTime());
+        }
+    }
+    IEnumerator SlowTime()
+    {
+        Time.timeScale = slowedTime;
+        Debug.Log("Slowed time.");
+        AudioManager.instance.PlayAudio(witchTime, "WitchTimeObject");
+        yield return new WaitForSeconds(slowmoTime);
+        Time.timeScale = 1;
+        Debug.Log("Time restored.");
     }
 }
